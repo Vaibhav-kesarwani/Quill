ba74e851ee2c4ccfc55af307b9c5c08175d9764f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-06-09 18:14:37 +0530

    add dependencies for -> utils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <MaxWidthWrapper className="bg-red-500">
+      <div></div>
+    </MaxWidthWrapper>
   );
 }
