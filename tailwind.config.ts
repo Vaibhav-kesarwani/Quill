df08d2e62506da3ee46a27c3c259d22cc6ed202f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-16 10:24:51 +0530

    change visbility page extension

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
+    // "./src/**/*.{ts,tsx}",
   ],
   theme: {
     container: {
