9b2039f497230aaf29dd55b19f5c5d3784e6d888
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-16 10:05:45 +0530

    Testing clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 export default function Home() {
   return (
     <MaxWidthWrapper className="bg-red-500">
-      <div></div>
+      <div>Hello World</div>
     </MaxWidthWrapper>
   );
 }
