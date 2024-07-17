3bc3013d8a560dbd3e676532331a480791f35679
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-17 11:17:08 +0530

    added the logic for an grainy class css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,6 +137,21 @@ export default function Home() {
             </div>
           </li>
         </ol>
+
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image
+                src="/file-upload-preview.jpg"
+                alt="uploading preview"
+                width={1419}
+                height={732}
+                quality={100}
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
