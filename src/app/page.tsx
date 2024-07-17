63db9b060e52439eaca2c03de66012f9aa0dc280
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-17 10:15:11 +0530

    added the heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ export default function Home() {
           Quill is now public!
         </p>
       </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+        Chat with your <span className="text-blue-600">documents</span> in seconds.
+      </h1>
     </MaxWidthWrapper>
   );
 }
